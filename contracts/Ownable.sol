618fc456e2c93c0c0212948eee2508cd3aa7925b
apecTheGreat <sirapecxd@gmail.com>
2022-07-23 20:36:28 +0300

    modified:   contracts/Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity > 0.4.0 < 0.9.0;
+pragma solidity ^0.8.4;
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
