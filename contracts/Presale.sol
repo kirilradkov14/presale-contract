49650fca61c4d4cff414dd668d95a2025758e4db
Kiril Radkov <sirapecxd@gmail.com>
2024-01-12 16:59:11 +0200

    modified: Presale.sol

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -21,7 +21,6 @@ contract Presale is Ownable(msg.sender) {
         IERC20 token;
         IUniswapV2Factory UniswapV2Factory;
         IUniswapV2Router02 UniswapV2Router02;
-        Pool pool;
         uint8 tokenDecimals;
     }
 
@@ -37,12 +36,65 @@ contract Presale is Ownable(msg.sender) {
         uint8 liquidityPortion;
     }
 
+    error PurchaseFail(string reason);
+
+    event Purchased(
+        address beneficiary, 
+        uint256 contribution, 
+        uint256 amount
+    );
+
     mapping(address => uint256) public ethContribution;
+
+    Data public data;
+    Pool public pool;
 
     constructor (
         Data memory _data,
         Pool memory _pool
         )  {
 
-        }
+    }
+
+    receive() external payable{}
+
+    function deposit() external onlyOwner {
+        
+    }
+
+    function finalize() external onlyOwner {
+        
+    }
+
+    function withdraw() external onlyOwner {
+        
+    }
+
+    function cancel() external onlyOwner {
+        
+    }
+
+    function claim() external {
+        
+    }
+
+    function refund() external {
+        
+    }
+
+    function purchase() public payable {
+        uint256 amount = msg.value;
+        _prevalidatePurchase(msg.sender, amount);
+        data.ethRaised += amount;
+        data.presaleTokens -= amount;
+        ethContribution[msg.sender] += amount;
+    }
+
+    function _prevalidatePurchase(address _beneficiary, uint256 _amount) internal view returns(bool) {
+        if(_amount == 0) revert PurchaseFail("Wei Amount is 0");
+        if(_amount < pool.minBuy) revert PurchaseFail("Purchase below min buy");
+        if(_amount + ethContribution[_beneficiary] > pool.maxBuy) revert PurchaseFail("Buy limit exceeded");
+        if(data.ethRaised > pool.hardCap) revert PurchaseFail("Hard cap reached");
+        return true;
+    }
 }
